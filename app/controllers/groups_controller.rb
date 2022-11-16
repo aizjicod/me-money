493bc1282b04969e973c4bc22e4b4c9b8b87152b
Aiziji <aletv1501@gmail.com>
2022-11-16 12:44:56 -0400

    Basic structure for categories page

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,7 +3,7 @@ class GroupsController < ApplicationController
 
   # GET /groups or /groups.json
   def index
-    @groups = Group.all
+    @groups = current_user.groups.all
   end
 
   # GET /groups/1 or /groups/1.json
@@ -20,7 +20,7 @@ class GroupsController < ApplicationController
   # POST /groups or /groups.json
   def create
     @group = Group.new(group_params)
-
+    @group.user_id = current_user.id
     respond_to do |format|
       if @group.save
         format.html { redirect_to group_url(@group), notice: 'Group was successfully created.' }
