4400e947e3edde1ae1fae9b4d2c2bd84187e248e
Aiziji <aletv1501@gmail.com>
2022-11-17 11:26:49 -0400

    Better styling for desktop on splash page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-
+  devise_scope :user do
+    get "/logout", :to => "devise/sessions#destroy" # Add a custom sing out route for user sign out
+ end
   authenticated :user do
     root to: 'groups#index', as: :authenticated_root
   end
